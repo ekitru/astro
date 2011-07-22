f7a3b67117748c863ed708bea56a774344207589
Kirill Trusov <trusov.kirill@gmail.com>
2011-07-22 12:39:11 +0300

    DbManager class added for holding db connection and all DB queries

--- Main.py
+++ Main.py
@@ -3,36 +3,35 @@
 import logging
 
 from configuration import Configuration, ConfigurationException
-from dbManager import getDbConnection
+from dbManager import DbManager
 from translations import Translate
 
 __author__ = 'kitru'
 
 if __name__ == '__main__':
     try:
-
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                             datefmt='%m-%d %H:%M',
                             filename='astroFull.log',
                             filemode='w')
 
-        logging.info('Program initialization')
+        logging.info('=== Program initialization ===')
         #Read configuration file
         configuration = Configuration('default.cnf')
 
         #Establish connection to DB
+        logging.info('=== DB initialization ===')
         dbConfig = configuration.getDbConfigDict()
-        db = getDbConnection(dbConfig)
-        cursor = db.cursor()
-        cursor.execute('select version()')
-        row = cursor.fetchone()
+        db = DbManager(dbConfig)
+        row = db.getVersion()
         if row is not None:
             print("Db connection established")
         else:
             print("Dd failed")
 
-#        Read selected language translation
+        #Read selected language translation
+        logging.info('=== Read translation page  ===')
         codes = configuration.getCodes()
         translate = Translate(codes)
         if translate.get("help"):
@@ -42,10 +41,11 @@ if __name__ == '__main__':
 
         translate.get('sfasdf')
     except ConfigurationException as ce:
-        print("Error during configuration occure:"+ce.__str__())
-    except Exception as e:
-        print(e.message)
-        print("Unexcepted error occur:" + e.__str__())
+        print("Error during configuration occure:" + ce.__str__())
+        raise ce
+        #    except Exception as e:
+    #        print("Unexcepted error occur:" + e.__str__())
+    #        raise e
 
     pass
   
--- dbManager.py
+++ dbManager.py
@@ -1,19 +1,30 @@
 import MySQLdb
-from configuration import ConfigurationException
+from configuration import ConfigurationException, getLogger
 
 __author__ = 'kitru'
 
-def getDbConnection(confDict):
-    """ Get db connection dased on config file
-    Attributes:
-        config - SafeConfigParser object
-    """
-    try:
-        db = MySQLdb.connect(confDict['host'], confDict['user'], confDict['password'], confDict['database'],
-                             port=int(confDict['port']))
-        return db
-    except Exception as error:
-        raise ConfigurationException(error.args)
+class DbManager(object):
+    def __init__(self, confDict):
+        self.logger = getLogger('astroDbManager')
+        self.logger.info('Establish connection')
+        self.conn = self.__getDbConnection(confDict)
 
+    def __getDbConnection(self, confDict):
+        """ Get db connection dased on config file
+        Attributes:
+            config - SafeConfigParser object
+        """
+        try:
+            db = MySQLdb.connect(confDict['host'], confDict['user'], confDict['password'], confDict['database'],
+                                 port=int(confDict['port']))
+            return db
+        except Exception as error:
+            raise ConfigurationException(self.logger, error.args)
+
+    def getVersion(self):
+        cursor = self.conn.cursor()
+        cursor.execute('select version()')
+        row = cursor.fetchone()
+        return row
 
   
