a0b2dca75fe3082867d1eeccf150f13574501e02
Kirill Trusov <trusov.kirill@gmail.com>
2011-08-19 15:32:37 +0300

    Mark out Exception classes

---
+++ Exceptions.py
@@ -0,0 +1,35 @@
+__author__ = 'kitru'
+
+class ConfigurationException(Exception):
+    """Exception raised for errors during configuration system.
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg, logger=None):
+        Exception.__init__(self, msg)
+        if logger:
+            logger.error(msg)
+
+
+class InitializationException(Exception):
+    """Exception raised for errors during system initialization.
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg, logger=None):
+        Exception.__init__(self, msg)
+        if logger:
+            logger.error(msg)
+
+class ClosingException(object):
+    """Exception raised for errors during system closing.
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg, logger=None):
+        Exception.__init__(self, msg)
+        if logger:
+            logger.error(msg)
