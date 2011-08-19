713e85f287f90b1a0580c4653938ac029015439a
Kirill Trusov <trusov.kirill@gmail.com>
2011-08-19 18:26:24 +0300

    star position will be stored in radians

--- db_init.sql
+++ db_init.sql
@@ -6,8 +6,8 @@ GRANT ALL ON astro.* TO 'astro'@'localhost';
 CREATE TABLE IF NOT EXISTS  `astro`.`stars`
 (`id` INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
  `name` VARCHAR(50) UNIQUE NOT NULL,
- `alfa` INTEGER NOT NULL,
- `delta` INTEGER NOT NULL
+ `alfa` FLOAT NOT NULL,
+ `delta` FLOAT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `astro`.`notes`
