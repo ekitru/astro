f4cc3ad2252e932418dcbdc6174e9f0e055fba0c
Kirill Trusov <trusov.kirill@gmail.com>
2011-09-06 21:52:22 +0300

    Merge branch 'master' of github.com:ekitru/astro
    Conflicts:
    	panels.py

--- panels.py
+++ panels.py
@@ -1,11 +1,17 @@
 import wx
-import os
 
 __author__ = 'kitru'
 
 class SimplePanel(wx.Panel):
     def __init__(self, parent=None, ID=wx.ID_ANY):
         wx.Panel.__init__(self, parent, ID)
+
+        #Sizes and Fonts
+        self.BUTTON_SIZE_SMALL = (30,25)
+        self.BUTTON_FONT_BOLD = wx.Font(10, wx.SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        self.IN_FIELD_SIZE_NORMAL = (85,27)
+        self.BUTTON_SIZE_LARGE = (85,27)
+        self.BUTTON_FONT_NORMAL = wx.Font(10, wx.SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
 
     def CreateHeader(self, name):
         return self.CreateElement(name, wx.Font(10, wx.SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
@@ -27,29 +33,25 @@ class SimplePanel(wx.Panel):
             element.SetFont(font)
         return element
 
-    def CreateInputField(self):
-        return wx.TextCtrl(self, id=wx.ID_ANY)
+    def CreateInputField(self, size=wx.DefaultSize):
+        return wx.TextCtrl(self, id=wx.ID_ANY, size=size)
 
     def CreateButton(self, label="",font=None, size=wx.DefaultSize):
-        speedSelButton = wx.Button(self, wx.ID_ANY, label=label, size=size)
+        speedSelButton = wx.Button(self, id=wx.ID_ANY, label=label, size=size)
         if font:
             speedSelButton.SetFont(font)
         return speedSelButton
 
     def CreateBitmapButton(self, rel_path, file_type, size=wx.DefaultSize):
-        bitmap = self.__gifBitmapWhiteBGToTransparent(rel_path, file_type)
+        bitmap = wx.Bitmap(rel_path, file_type)
         return wx.BitmapButton(self, id=wx.ID_ANY, bitmap=bitmap,size=size)
 
-    def __gifBitmapWhiteBGToTransparent(self,path,type):
-        """Turns bitmap with white backgrount into
-        bitmap with transparent background.
-        path - relative path to bitmap file.
-        type - type of bitmap file (.gif)"""
-        absPath = os.path.abspath(path)
-        bitmap = wx.Bitmap(absPath, type=type)
-        mask = wx.MaskColour(bitmap, wx.WHITE)
-        bitmap.SetMask(mask)
-        return bitmap
+    def CreateToggleButton(self, label="", font=None ,size=wx.DefaultSize):
+        toggleButton = wx.ToggleButton(self, id=wx.ID_ANY, label=label, size=size)
+        if font:
+            toggleButton.SetFont(font)
+        return toggleButton
+
 
 class ObjectPanel(SimplePanel):
     """This panel represents selected object data (name and positions)
@@ -97,8 +99,6 @@ class ObjectPanel(SimplePanel):
         objPos = self.CreateObjectPosition(codes)
         sizer.Add(objPos, flag=wx.ALL, border=10)
 
-        self.moveBut = wx.Button(self, wx.ID_ANY, codes.get('pObjMove'))
-        sizer.Add(self.moveBut, flag=wx.ALIGN_RIGHT)
         return sizer
 
     def CreateObjectPosition(self, codes):
@@ -121,8 +121,6 @@ class ObjectPanel(SimplePanel):
     def update(self, controller):
         """Updates Objects name and coordinates
         """
-
-        self.moveBut.Enable(controller.scopeCanMove())
 
         object = controller.getObject()
         data = object.getData()
@@ -179,21 +177,15 @@ class TimeDatePanel(SimplePanel):
 
 
 class PositioningPanel(SimplePanel):
-    """This panel represents telescope position with manual positioning opportunities
+    """This panel represents telescope position: current and end coordinates
     Attributes:
         codes - Translation codes
     """
-
     def __init__(self, parent, ID=wx.ID_ANY, codes=None):
         SimplePanel.__init__(self, parent, ID)
 
-        bitmapButtonSize = (30,23)
-        speedButtonSize = (30,23)
-        speedButtonFont = wx.Font(18, wx.SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
-        speedHourButtonFont = wx.Font(10, wx.SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
-        bitmapType = wx.BITMAP_TYPE_GIF
-
-        posSizer = wx.GridSizer(4, 3, 5, 10)
+        #Positioning panel view sizer
+        pPosViewSizer = wx.GridSizer(4, 3, 5, 10)
 
         self.curRA = self.CreateField()
         self.taskRA = self.CreateField()
@@ -201,54 +193,28 @@ class PositioningPanel(SimplePanel):
         self.taskDEC = self.CreateField()
         self.curFocus = self.CreateField()
         self.taskFocus = self.CreateField()
-        self.control = wx.Button(self, wx.ID_ANY, codes.get('pPosCtrl'),size=(90,23))
 
-        posSizer.Add(self.CreateField())
-        posSizer.Add(self.CreateCaption(codes.get('pPosCur')), flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
-        posSizer.Add(self.CreateCaption(codes.get('pPosEnd')), flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
+        pPosViewSizer.Add(self.CreateField())
+        pPosViewSizer.Add(self.CreateCaption(codes.get('pPosCur')), flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
+        pPosViewSizer.Add(self.CreateCaption(codes.get('pPosEnd')), flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
 
-        posSizer.Add(self.CreateCaption(codes.get('pPosRA')), flag=wx.ALL | wx.ALIGN_RIGHT)
-        posSizer.Add(self.curRA, flag=wx.ALL | wx.ALIGN_CENTER)
-        posSizer.Add(self.taskRA, flag=wx.ALL | wx.ALIGN_CENTER)
+        pPosViewSizer.Add(self.CreateCaption(codes.get('pPosRA')), flag=wx.ALL | wx.ALIGN_RIGHT)
+        pPosViewSizer.Add(self.curRA, flag=wx.ALL | wx.ALIGN_CENTER)
+        pPosViewSizer.Add(self.taskRA, flag=wx.ALL | wx.ALIGN_CENTER)
 
-        posSizer.Add(self.CreateCaption(codes.get('pPosDEC')), flag=wx.ALL | wx.ALIGN_RIGHT)
-        posSizer.Add(self.curDEC, flag=wx.ALL | wx.ALIGN_CENTER)
-        posSizer.Add(self.taskDEC, flag=wx.ALL | wx.ALIGN_CENTER)
+        pPosViewSizer.Add(self.CreateCaption(codes.get('pPosDEC')), flag=wx.ALL | wx.ALIGN_RIGHT)
+        pPosViewSizer.Add(self.curDEC, flag=wx.ALL | wx.ALIGN_CENTER)
+        pPosViewSizer.Add(self.taskDEC, flag=wx.ALL | wx.ALIGN_CENTER)
 
-        posSizer.Add(self.CreateCaption(codes.get('pPosFoc')), flag=wx.ALL | wx.ALIGN_RIGHT)
-        posSizer.Add(self.curFocus, flag=wx.ALL | wx.ALIGN_CENTER)
-        posSizer.Add(self.taskFocus, flag=wx.ALL | wx.ALIGN_CENTER)
+        pPosViewSizer.Add(self.CreateCaption(codes.get('pPosFoc')), flag=wx.ALL | wx.ALIGN_RIGHT)
+        pPosViewSizer.Add(self.curFocus, flag=wx.ALL | wx.ALIGN_CENTER)
+        pPosViewSizer.Add(self.taskFocus, flag=wx.ALL | wx.ALIGN_CENTER)
 
+        #Positioning panel sizer
+        pPosSizer = wx.StaticBoxSizer(wx.StaticBox(self, label=codes.get('pPos')), wx.VERTICAL)
+        pPosSizer.Add(pPosViewSizer, flag=wx.ALL, border=10)
 
-        plcSizer = wx.FlexGridSizer(4, 5, 5, 5)
-        RAspeedSelSizer = wx.BoxSizer(wx.HORIZONTAL)
-        DECspeedSelSizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.manSetPointRA = self.CreateInputField()
-        self.movLeftRA = self.CreateBitmapButton('bitmaps/arrow_left.gif', wx.BITMAP_TYPE_GIF, size=bitmapButtonSize)
-        self.movRightRA = self.CreateBitmapButton('bitmaps/arrow_right.gif', wx.BITMAP_TYPE_GIF, size=bitmapButtonSize)
-        self.speedSecRA = self.CreateButton(label=codes.get('pPosSpeedSec'), font=speedButtonFont, size=speedButtonSize)
-        self.speedMinRA = self.CreateButton(label=codes.get('pPosSpeedMin'), font=speedButtonFont, size=speedButtonSize)
-        self.speedHourRA = self.CreateButton(label=codes.get('pPosSpeedHour'),size=speedButtonSize,
-                                             font=speedHourButtonFont)
-
-        RAspeedSelSizer.Add(self.speedHourRA)
-        RAspeedSelSizer.Add(self.speedMinRA)
-        RAspeedSelSizer.Add(self.speedSecRA)
-
-        plcSizer.Add(self.CreateCaption(codes.get('pPosRA')), flag=wx.ALIGN_CENTER_VERTICAL | wx.LEFT, border=10)
-        plcSizer.Add(self.manSetPointRA, border= 10)
-        plcSizer.Add(self.movLeftRA)
-        plcSizer.Add(self.movRightRA)
-        plcSizer.Add(RAspeedSelSizer)
-
-
-        comSizer = wx.StaticBoxSizer(wx.StaticBox(self, label=codes.get('pPos')), wx.VERTICAL)
-        comSizer.Add(posSizer, flag=wx.ALL, border=10)
-        comSizer.Add(self.control, flag=wx.ALIGN_RIGHT)
-        comSizer.Add(plcSizer, flag = wx.TOP, border=10)
-
-        self.SetSizer(comSizer)
+        self.SetSizer(pPosSizer)
 
     def update(self, controller):
         position = controller.getTelescopePosition()
@@ -261,6 +227,78 @@ class PositioningPanel(SimplePanel):
         self.taskRA.SetLabel(position['end'][0])
         self.taskDEC.SetLabel(position['end'][1])
         self.taskFocus.SetLabel(focus['end'])
+
+class ControlPanel(SimplePanel):
+    """This panel represents manual control of the telescope. Auto or Manual control modes can be selected.
+    Attributes:
+        codes - Translation codes
+    """
+    def __init__(self, parent, ID=wx.ID_ANY, codes=None):
+        SimplePanel.__init__(self,parent,ID)
+
+        #Positioning panel control sizer
+        pControlSizer = wx.BoxSizer(wx.HORIZONTAL)
+        pControlCol1 = wx.BoxSizer(wx.VERTICAL)
+        pControlCol2 = wx.BoxSizer(wx.VERTICAL)
+        pControlCol3 = wx.GridSizer(4,3,5,5)
+
+        self.inFieldRA = self.CreateInputField(size=self.IN_FIELD_SIZE_NORMAL)
+        self.inFieldDEC = self.CreateInputField(size=self.IN_FIELD_SIZE_NORMAL)
+        self.inFieldFoc = self.CreateInputField(size=self.IN_FIELD_SIZE_NORMAL)
+        self.autoManBut = self.CreateButton(codes.get('pCtrlMan'),font=self.BUTTON_FONT_NORMAL,size=self.BUTTON_SIZE_LARGE)
+        self.butManMove = self.CreateButton(label=codes.get('pCtrlMov'))
+        self.butMovUpRA = self.CreateBitmapButton("bitmaps/arrow_up.ico", file_type=wx.BITMAP_TYPE_ICO, size = self.BUTTON_SIZE_SMALL)
+        self.butMovLftDEC = self.CreateBitmapButton("bitmaps/arrow_left.ico", file_type=wx.BITMAP_TYPE_ICO, size = self.BUTTON_SIZE_SMALL)
+        self.butMovRhtDEC = self.CreateBitmapButton("bitmaps/arrow_right.ico", file_type=wx.BITMAP_TYPE_ICO, size = self.BUTTON_SIZE_SMALL)
+        self.butMovDwnRA =  self.CreateBitmapButton("bitmaps/arrow_down.ico", file_type=wx.BITMAP_TYPE_ICO, size = self.BUTTON_SIZE_SMALL)
+        self.butSelHour = self.CreateToggleButton(label=codes.get('pCtrlHour'),font=self.BUTTON_FONT_BOLD, size=self.BUTTON_SIZE_SMALL)
+        self.butSelMin = self.CreateToggleButton(label=codes.get('pCtrlMin'),font=self.BUTTON_FONT_BOLD, size=self.BUTTON_SIZE_SMALL)
+        self.butSelSec = self.CreateToggleButton(label=codes.get('pCtrlSec'),font=self.BUTTON_FONT_BOLD, size=self.BUTTON_SIZE_SMALL)
+        self.butIncFoc = self.CreateButton(label="+", size=self.BUTTON_SIZE_SMALL)
+        self.butDecFoc = self.CreateButton(label="-", size=self.BUTTON_SIZE_SMALL)
+
+        pControlCol1.AddSpacer(5)
+        pControlCol1.Add(self.CreateCaption(codes.get('pCtrlRA')), flag=wx.ALIGN_RIGHT)
+        pControlCol1.AddSpacer(11)
+        pControlCol1.Add(self.CreateCaption(codes.get('pCtrlDEC')), flag=wx.ALIGN_RIGHT)
+        pControlCol1.AddSpacer(12)
+        pControlCol1.Add(self.CreateCaption(codes.get('pCtrlFoc')), flag=wx.ALIGN_RIGHT)
+        pControlCol1.AddSpacer(12)
+        pControlCol1.Add(self.autoManBut, flag=wx.ALIGN_RIGHT)
+
+        pControlCol2.Add(self.inFieldRA)
+        pControlCol2.AddSpacer(1)
+        pControlCol2.Add(self.inFieldDEC)
+        pControlCol2.AddSpacer(2)
+        pControlCol2.Add(self.inFieldFoc)
+        pControlCol2.AddSpacer(4)
+        pControlCol2.Add(self.butManMove)
+        #self.moveBut.Enable(controller.scopeCanMove())
+
+        pControlCol3.AddSpacer(5)
+        pControlCol3.Add(self.butMovUpRA)
+        pControlCol3.AddSpacer(5)
+        pControlCol3.Add(self.butMovLftDEC)
+        pControlCol3.Add(self.butMovDwnRA)
+        pControlCol3.Add(self.butMovRhtDEC)
+        pControlCol3.Add(self.butDecFoc)
+        pControlCol3.AddSpacer(5)
+        pControlCol3.Add(self.butIncFoc)
+        pControlCol3.Add(self.butSelHour)
+        pControlCol3.Add(self.butSelMin)
+        pControlCol3.Add(self.butSelSec)
+
+        pControlSizer.AddSpacer(10)
+        pControlSizer.Add(pControlCol1)
+        pControlSizer.AddSpacer(25)
+        pControlSizer.Add(pControlCol2)
+        pControlSizer.AddSpacer(20)
+        pControlSizer.Add(pControlCol3)
+
+        comSizer = wx.StaticBoxSizer(wx.StaticBox(self, label=codes.get('pCtrl')), wx.VERTICAL)
+        comSizer.Add(pControlSizer, flag=wx.ALL, border=10)
+
+        self.SetSizer(comSizer)
 
 
 class TelescopePanel(SimplePanel):    #TODO decide, what to do with it, temp mock
