4e493e4b2b956319a5f2ebbf1f5b9a71f92c64db
Kirill Trusov <trusov.kirill@gmail.com>
2011-09-16 14:30:45 +0300

    added current position and focus writing

--- LogThread.py
+++ LogThread.py
@@ -4,13 +4,14 @@ from db import Message, Log, Star
 __author__ = 'kitru'
 
 class LogThread(object):
-    __scale=1
-    def __init__(self,  controller, minutes=1):
+    __scale = 1
+
+    def __init__(self, controller, minutes=1):
         self.__period = minutes * self.__scale
         self.__controller = controller
-        self.__star = Star(controller.getDbManager())
         self.__log = Log(controller.getDbManager())
         self.__message = Message(controller.getDbManager())
+        self.__plc = controller.PLCManager
         self.start()
 
     def start(self):
@@ -23,7 +24,8 @@ class LogThread(object):
     def doWork(self):
         self.__log.setStarId(self.getStarId())
         self.__log.setMsgId(self.getMsgId())
-
+        self.__log.setCurrentRaDec(*self.getCurrentRaDec())
+        self.__log.setCurrentFocus(self.getCurrentFocus())
         self.__log.saveLog()
         self.start()
 
@@ -34,3 +36,17 @@ class LogThread(object):
     def getMsgId(self):
         id = self.__message.getLastId()
         return id
+
+    def getCurrentRaDec(self):
+        position = self.__plc.getPosition()
+        return position[0]
+
+    def getCurrentFocus(self):
+        focus = self.__plc.getFocus()
+        return focus[0]
+
+    def getTemperature(self, temp_in, temp_out):
+        pass #PLC
+
+    def getAlarmStatus(self, word):
+        pass #PLC
