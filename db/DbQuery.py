6c491aa45b8d86d478a8f0c3f82b5b386272ed15
Kirill Trusov <trusov.kirill@gmail.com>
2011-10-07 11:29:12 +0300

    Code organization: move db.py into db package and split into several independant files

--- db/DbQuery.py
+++ db/DbQuery.py
@@ -1,57 +1,8 @@
-# -*- coding: utf-8 -*-
 import threading
-
-import MySQLdb
 import time
 import astronomy
 
-from Exceptions import ConfigurationException, DbException
-from logger import getLog, closeLog
-
-
-__author__ = 'kitru'
-
-class DbManager(object):
-    """ Manage DB connection
-    handle it opening and closing, also handle logs """
-
-    def __init__(self, confDict):
-        self._logger = getLog('database')
-        self._dbName = confDict['database']
-        self._db = self._getDbConnection(confDict)
-
-    def __del__(self):
-        self.close()
-        closeLog(self._logger)
-
-    def _getDbConnection(self, confDict):
-        """ Get db connection dased on config file
-        Attributes:
-            config - SafeConfigParser object
-        """
-        try:
-            self._logger.info('Establishing connection')
-            db = MySQLdb.connect(confDict['host'], confDict['user'], confDict['password'], confDict['database'],
-                                 port=int(confDict['port']), charset="utf8", use_unicode=True)
-            self._logger.info('Connection established')
-            return db
-        except Exception as error:
-            raise ConfigurationException(error.args, self._logger)
-
-    def getDb(self):
-        return self._db
-
-    def getLog(self):
-        return self._logger
-
-    def close(self):
-        """ Closes DB connection. If it clsoed logs event and do nothing """
-        if self._db:
-            self._logger.info("Close DB connection")
-            self._db.close()
-        else:
-            self._logger.info("DB connection already closed")
-
+from Exceptions import DbException
 
 class DBQuery(object):
     """ Simple db helper, makes easier data manipulations. Thread safe  """
---
+++ db/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'kitru'
+
+from db import DbManager
+from db import DbQuery
+
+DbManager = DbManager.DbManager
+
+Message = DbQuery.Message
+Star = DbQuery.Star
+Log = DbQuery.Log
+
