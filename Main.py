6f6e38a9641f5715765d40a7873fd2301feebe46
Kirill Trusov <trusov.kirill@gmail.com>
2011-10-10 23:20:11 +0300

    added gui exception catching

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.6 -B
 # -*- coding: UTF-8 -*-
 from core.Controller import Controller
-from core.Exceptions import InitializationException, ClosingException
+from core.Exceptions import InitializationException, ClosingException, GuiException
 
 import wx
 from gui.MainGui import MainGui
@@ -21,6 +21,9 @@ if __name__ == '__main__':
         controller.freeResources()
     except InitializationException as exception:
         print("Unexcepted error occured during resources initialization: " + exception.__str__())
+        raise exception
+    except GuiException as exception:
+        print("GUI finished by exception: " + exception.__str__())
         raise exception
     except ClosingException as exception:
         print("Unexcepted error occured during resources closing: " + exception.__str__())
