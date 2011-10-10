65457c4bd644da0b71b32f285d3c90291559e507
Kirill Trusov <trusov.kirill@gmail.com>
2011-10-11 00:49:59 +0300

    create and use astro exception

--- core/Exceptions.py
+++ core/Exceptions.py
@@ -1,60 +1,55 @@
 __author__ = 'kitru'
 
-class ConfigurationException(Exception):
+class AstroException(Exception):
+    def __init__(self, msg, logger=None):
+        Exception.__init__(self, msg)
+        if logger:
+            logger.exception(msg)
+
+class ConfigurationException(AstroException):
     """Exception raised for errors during configuration system.
     Attributes:
         msg  -- explanation of the error
     """
 
     def __init__(self, msg, logger=None):
-        Exception.__init__(self, msg)
-        if logger:
-            logger.error(msg)
+        AstroException.__init__(self, msg, logger)
 
 
-class InitializationException(Exception):
+class InitializationException(AstroException):
     """Exception raised for errors during system initialization.
     Attributes:
         msg  -- explanation of the error
     """
 
     def __init__(self, msg, logger=None):
-        Exception.__init__(self, msg)
-        if logger:
-            logger.error(msg)
+        AstroException.__init__(self, msg, logger)
 
 
-class ClosingException(Exception):
+class ClosingException(AstroException):
     """Exception raised for errors during system closing.
     Attributes:
         msg  -- explanation of the error
     """
 
     def __init__(self, msg, logger=None):
-        Exception.__init__(self, msg)
-        if logger:
-            logger.error(msg)
+        AstroException.__init__(self, msg, logger)
 
 
-class DbException(Exception):
+class DbException(AstroException):
     """Exception raised if Db return Exception.
     Attributes:
         msg  -- explanation of the error
     """
 
     def __init__(self, msg, logger=None):
-        Exception.__init__(self, msg)
-        if logger:
-            logger.error(msg)
+        AstroException.__init__(self, msg, logger)
 
-class GuiException(Exception):
+class GuiException(AstroException):
     """Exception raised if GUI return Exception.
     Attributes:
         msg  -- explanation of the error
     """
 
     def __init__(self, msg, logger=None):
-        Exception.__init__(self, msg)
-        if logger:
-            logger.error(msg)
-
+        AstroException.__init__(self, msg, logger)
