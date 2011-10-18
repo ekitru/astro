c59f1a685df74ccc47c1985e9257619306ba9b2f
Kirill Trusov <trusov.kirill@gmail.com>
2011-10-18 18:12:44 +0300

    small test program for PLC

--- plc_test.py
+++ plc_test.py
@@ -6,3 +6,7 @@ __author__ = 'kitru'
 if __name__ == '__main__':
     plc = PLCManager()
     plc.test()
+    number = float(1.23456789)
+    plc.writeCoordinate(10, number)
+    number = plc.readCoordinate(20)
+    print(number)
