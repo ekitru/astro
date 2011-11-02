8d3c38408316cf9561cbefd791ad2f09ab738b23
Kirill Trusov <trusov.kirill@gmail.com>
2011-11-02 14:38:20 +0200

    takes and realing control added

--- gui/panels/ControlModePanel.py
+++ gui/panels/ControlModePanel.py
@@ -10,12 +10,13 @@ class ControlModePanel(SimplePanel):
         codes - Translation codes
     """
     _setpointControlMode = 1
+
     def __init__(self, parent, codes, resources, controls):
         """ Attr:
             codes - translation codes
             resouces - program resources
             controls - ManualSetPointPanel """
-        SimplePanel.__init__(self,parent)
+        SimplePanel.__init__(self, parent)
 
         self._resources = resources
         self._controls = controls
@@ -42,37 +43,42 @@ class ControlModePanel(SimplePanel):
 
         self.Bind(wx.EVT_RADIOBUTTON, self.OnObjSetpointRadBut, id=self.rbObjectSetpoint.GetId())
         self.Bind(wx.EVT_RADIOBUTTON, self.OnManSetpointRadBut, id=self.rbManualSetpoint.GetId())
-        self.Bind(wx.EVT_RADIOBUTTON, self.OnRemoteCtrlRadBut, id=self.rbRemoteControl.GetId())
         self.Bind(wx.EVT_BUTTON, self.OnButtonMove, self.butMove)
 
     def OnObjSetpointRadBut(self, event):
+        self.takeControl()
         self._controls.Hide()
+        self.GetParent().Fit()
         self._resources.updateSetPoint()
 
     def OnManSetpointRadBut(self, event):
+        self.takeControl()
         self._controls.Show()
         self.GetParent().Fit()
 
-    def OnRemoteCtrlRadBut(self, event):
-        return
+    def takeControl(self):
+        plc = self._resources.getPLCManager()
+        plc.takeControl()
 
     def OnButtonMove(self, event):
         event.Skip()
         data = self._resources.getSetPoint().getRawData()
-        print(data)
         plc = self._resources.getPLCManager()
         plc.setSetpointPosition(data['ra'], data['dec'])
         if data['focus']:
             plc.setFocus(data['focus'])
 
+        plc.startMoving()
+
     def update(self, controller):
+        mode = self._resources.getPLCManager().readControlMode()
+        self.rbRemoteControl.SetValue(self._isRemoveControl(mode))
+
         if self.rbObjectSetpoint.GetValue():
             self._resources.updateSetPoint()
 
         if self.rbManualSetpoint.GetValue():
             self._controls.updateSetPoint()
 
-        if controller.remoteControlSelected():
-            self.rbRemoteControl.SetValue(True)
-        else:
-            self.rbRemoteControl.SetValue(False)
+    def _isRemoveControl(self, mode):
+        return mode != 1
