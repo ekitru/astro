accc004375231d36027f249775cb40f98cc33f7a
Kirill Trusov <trusov.kirill@gmail.com>
2011-11-20 15:27:19 +0200

    small clean up

--- core/LogThread.py
+++ core/LogThread.py
@@ -12,16 +12,14 @@ class LogThread(object):
 
     def __init__(self, resources):
         try:
+            self._mutex = threading.RLock()
             logging.info('Starting logging thread')
             self._resources = resources
             db = self._resources.getDbManager()
             self._log = Log(db)
             self._message = Message(db)
-
             self._plc = self._resources.plcManager
-
             self._period = resources.getConfig().getLoggingTime()
-            self._mutex = threading.RLock()
             self._start()
         except Exception as ex:
             raise ConfigurationException(ex.args)
@@ -43,12 +41,12 @@ class LogThread(object):
     def _doWork(self):
         """ All logging stuff performs here. This method is calling by logging thread """
         with self._mutex:
-            self._log.setMsgId(self.getMsgId())
-            self._log.setCurrentRaDec(*self.getCurrentRaDec())
-            self._log.setCurrentFocus(self.getCurrentFocus())
-            self._log.setStarId(self.getStarId())
-            self._log.setTemperature(*self.getTemperature())
-            self._log.setAlarmStatus(self.getAlarmStatus())
+            self._log.setStarId(self._getStarId())
+            self._log.setMsgId(self._getMsgId())
+            self._log.setCurrentRaDec(*self._getCurrentRaDec())
+            self._log.setCurrentFocus(self._getCurrentFocus())
+            self._log.setTemperature(*self._getTemperature())
+            self._log.setAlarmStatus(self._getAlarmStatus())
             self._log.writeToLog()
 
     def force(self):
@@ -61,30 +59,31 @@ class LogThread(object):
         """ Update logging period, period in seconds """
         self._period = time
 
-    def getStarId(self):
+    def _getStarId(self):
         """ return selected object id from controller, if object is not selected return None """
         object = self._resources.getObject()
         return object.getId()
 
-    def getMsgId(self):
+    def _getMsgId(self):
         """ for getting current message last stored message is used """
         id = self._message.getLastId()
         return id
 
-    def getCurrentRaDec(self):
+    def _getCurrentRaDec(self):
         """ current telescope positions are taken directly from plc """
         return self._plc.getCurrentPosition()
 
-    def getCurrentFocus(self):
+    def _getCurrentFocus(self):
         """ current telescope positions are taken directly from plc """
         focus = self._plc.getFocus()
         return focus[0]
 
-    def getTemperature(self):
+    def _getTemperature(self):
+        """ current dome and telescope temperatures """
         temp = self._plc.readTemperature()
         return temp['pState_tempT'], temp['pState_tempD']
 
-    def getAlarmStatus(self):
-#        return 'fa'
+    def _getAlarmStatus(self):
+        """ alarm status as a alarm codes separated by ',' """
         return self._plc.readAlarmStatus()
 
