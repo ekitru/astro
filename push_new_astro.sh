009e3bd5222b37f50c2e4d6b06368dba01f167bd
Kirill Trusov <trusov.kirill@gmail.com>
2012-01-16 00:18:10 +0200

    Script, for deploy program

---
+++ push_new_astro.sh
@@ -0,0 +1,7 @@
+cd $HOME/astro_project
+sudo rm -rf /home/user/astro_project/*
+sudo cp -R . /home/user/astro_project/.
+sudo chown -R user:user /home/user/astro_project
+sudo rm -rf /home/user/astro_project/.idea
+sudo rm -rf /home/user/astro_project/.git
+sudo rm -rf /home/user/astro_project/astro_project.iml
