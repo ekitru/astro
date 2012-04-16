4853ec6ea11a1d487ce7debbcfda4eeec1bc6e90
Kirill Trusov <trusov.kirill@gmail.com>
2012-04-16 19:35:59 +0300

    add altitude column into log table

--- db/Log.py
+++ db/Log.py
@@ -25,20 +25,19 @@ class Log(DBQuery):
         self.insert(sql, args)
 
     def _getSQL(self):
-        fields = "(`id`, `time`, `star_id`, `msg_id`, `ra`, `dec`, `focus`,`temp_in`, `temp_out`,`status`)"
-        values = "(default, %(time)s, %(star_id)s, %(msg_id)s, %(ra)s, %(dec)s, %(focus)s, %(temp_in)s, %(temp_out)s, %(status)s)"
+        fields = "(`id`, `time`, `star_id`, `msg_id`, `ra`, `dec`, `altitude`, `focus`,`temp_in`, `temp_out`,`status`)"
+        values = "(default, %(time)s, %(star_id)s, %(msg_id)s, %(ra)s, %(dec)s, %(altitude)s, %(focus)s, %(temp_in)s, %(temp_out)s, %(status)s)"
         sql = "INSERT INTO `log` " + fields + " VALUES " + values
         return sql
 
     def _getArgs(self):
         timestamp = int(time.time())
         return {'id': self._id, 'time': timestamp, 'star_id': self._star_id, 'msg_id': self._msg_id, 'ra': self._ra,
-                'dec': self._dec,
-                'focus': self._focus, 'temp_in': self._temp_in, 'temp_out': self._temp_out, 'status': self._status}
+                'dec': self._dec, 'altitude': self._alt, 'focus': self._focus, 'temp_in': self._temp_in, 'temp_out': self._temp_out, 'status': self._status}
 
     def readLog(self, starName=None, startDate=None, endDate=None):
         """ Read log, result can be filtered by star name or logging period """
-        select = "SELECT l.`id`,l.`time`, s.`name`, s.`ra`,s.`dec`, m.`text`, l.`ra`, l.`dec`, l.`focus`, l.`temp_in`, l.`temp_out`, l.`status` FROM `log` l LEFT JOIN `star` s ON l.star_id=s.id LEFT JOIN `message` m ON l.msg_id=m.id"
+        select = "SELECT l.`id`,l.`time`, s.`name`, s.`ra`,s.`dec`, m.`text`, l.`ra`, l.`dec`, l.`altitude`, l.`focus`, l.`temp_in`, l.`temp_out`, l.`status` FROM `log` l LEFT JOIN `star` s ON l.star_id=s.id LEFT JOIN `message` m ON l.msg_id=m.id"
         condition = self.conditionConstruct(starName, startDate, endDate)
         rows = self.selectAll(select, where=condition)
         list = []
@@ -50,9 +49,10 @@ class Log(DBQuery):
             data['sRa'], data['sDec'] = astronomy.rad2str(row[3], row[4])
             data['msg'] = row[5]
             data['ra'], data['dec'] = astronomy.rad2str(row[6], row[7])
-            data['focus'] = row[8]
-            data['temp_in'], data['temp_out'] = row[9], row[10]
-            data['status'] = row[11]
+            data['alt'] = row[8]
+            data['focus'] = row[9]
+            data['temp_in'], data['temp_out'] = row[10], row[11]
+            data['status'] = row[12]
             #print('get from DB', data)
             list.append(data)
         self._log = list
@@ -79,7 +79,8 @@ class Log(DBQuery):
         #Parameters from PLC
         self._ra = None
         self._dec = None
-        self._focus = 123
+        self._alt = None
+        self._focus = None
         self._temp_in = None
         self._temp_out = None
         self._status = None
@@ -93,6 +94,9 @@ class Log(DBQuery):
     def setCurrentRaDec(self, ra, dec):
         self._ra = ra
         self._dec = dec
+
+    def setCurrentAltitude(self, alt):
+        self._alt = alt
 
     def setCurrentFocus(self, focus):
         self._focus = focus
