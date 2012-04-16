c015b2288dcc9ae366d2594066c9a0c779f9e8ee
Kirill Trusov <trusov.kirill@gmail.com>
2012-04-16 22:46:19 +0300

    changes in deployment script

--- push_new_astro.sh
+++ push_new_astro.sh
@@ -4,4 +4,5 @@ sudo cp -R . /home/user/astro_project/.
 sudo chown -R user:user /home/user/astro_project
 sudo rm -rf /home/user/astro_project/.idea
 sudo rm -rf /home/user/astro_project/.git
+sudo rm -rf /home/user/astro_project/.gitignore
 sudo rm -rf /home/user/astro_project/astro_project.iml
