63aa711c8e0a82b34e721aa7262378f61f605bb3
Kirill Trusov <trusov.kirill@gmail.com>
2012-04-16 22:56:33 +0300

    remove unneeded files

--- push_new_astro.sh
+++ push_new_astro.sh
@@ -6,3 +6,6 @@ sudo rm -rf /home/user/astro_project/.idea
 sudo rm -rf /home/user/astro_project/.git
 sudo rm -rf /home/user/astro_project/.gitignore
 sudo rm -rf /home/user/astro_project/astro_project.iml
+sudo rm -rf /home/user/astro_project/README
+sudo rm -rf /home/user/astro_project/push_new_astro.sh
+
